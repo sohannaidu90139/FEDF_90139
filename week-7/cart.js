f22b94e9a446445f9e0a2353ef8d225d6ed0cf3e
sohannaidu90139 <2420090139@klh.edu.in>
2025-09-24 09:26:40 +0530

    Add files via upload

---
+++ week-7/cart.js
@@ -0,0 +1,15 @@
+export let cart = [];
+
+
+export function addToCart(book) {
+    cart.push(book);
+}
+
+
+export function removeFromCart(index) {
+    cart.splice(index, 1);
+}
+
+export function calculateTotal() {
+    return cart.reduce((sum, book) => sum + book.price, 0);
+}
---
+++ week-7/data.js
@@ -0,0 +1,7 @@
+export const books = 
+[
+    { title: "JavaScript Essentials", author: "Hemanth sai", price: 499, availability: "in stock" },
+    { title: "Learn React", author: "Jane Smith", price: 699, availability: "in stock" },
+    { title: "Backend Essentials", author: "Mark Lee", price: 599, availability: "out of stock" },
+    { title: "FEDF", author: "Ramya Krishna", price: 450, availability: "in stock" }
+];
